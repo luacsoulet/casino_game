b4d44af149a8472851fac5295cf333c14c67e8be
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-05-20 16:03:46 +0200

    backend init

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,6 @@
+export interface User {
+    username: string;
+    password: string;
+    virtual_balance: number;
+}
+
---
+++ backend/src/server.ts
@@ -0,0 +1,47 @@
+import http from 'http';
+import app from './app';
+
+const normalizePort = (val: string) => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+
+const port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
+
+const errorHandler = (error: any) => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+        default:
+            throw error;
+    }
+}
+
+const server = http.createServer(app);
+server.on('error', errorHandler);
+server.on('listening', onListening);
+
+function onListening() {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind + ' ✅');
+}
+
+server.listen(port);
