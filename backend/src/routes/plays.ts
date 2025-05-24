d32d12c5930a23545d30778959f0c544a24d7e6c
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-05-24 18:21:20 +0200

    Add history of plays routes

--- backend/src/routes/plays.ts
+++ backend/src/routes/plays.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createPlay } from '../controllers/playsControllers';
+import { createPlay, getPlayHistory } from '../controllers/playsControllers';
 import { auth } from '../middleware/auth';
 
 const router = express.Router();
 
 router.post('/:gameId', auth, createPlay);
+router.get('/history', auth, getPlayHistory);
 
 export default router; 
