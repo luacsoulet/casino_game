e9df0e29ac1e8bd4b59bd001e49252729c5bb6ed
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-05-24 18:22:27 +0200

    Add new users routes
    - Get All users
    - Modify balance from specified user
    - Delete User

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { getUserProfile, getUserPlays } from '../controllers/usersControllers';
+import { getUserProfile, getUserPlays, getUsers, deleteUser, modifyUserBalance } from '../controllers/usersControllers';
+import { auth } from '../middleware/auth';
+import { isAdmin } from '../middleware/isAdmin';
 
 const router = express.Router();
 
+router.get('/', auth, isAdmin, getUsers);
 router.get('/:id', getUserProfile);
 router.get('/:id/plays', getUserPlays);
+router.put('/:id/balance', auth, isAdmin, modifyUserBalance);
+router.delete('/:id', auth, isAdmin, deleteUser);
 
 export default router;
